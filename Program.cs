f58ce997128797995a0598c8eee00ce70e43ebd2
Stanimir Raykov <100510232+StanimirRaykov@users.noreply.github.com>
2024-11-13 19:56:41 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using Database_Management_System;
+
+CommandParser commandParser = new CommandParser();
+Console.WriteLine("Welcome!");
+
+while(true)
+{
+    Console.Write("DBMS> ");
+    string command = Console.ReadLine();
+    commandParser.ParseCommand(command);
+}
